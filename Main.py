c2bf9be41cafcba53886707f347a03e86f3c394c
NigelDeLeon20 <craignigeldeleon20@gmail.com>
2026-09-08 17:46:40 +0800

    Added certificates, contact, index, projects, resume htmls

--- Main.py
+++ Main.py
@@ -4,7 +4,23 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello, World!"
+    return render_template("index.html")
+
+@app.route("/projects")
+def project():
+    return render_template("projects.html")
+
+@app.route("/certificates")
+def certificates():
+    return render_template("certificates.html")
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+@app.route("/resume")
+def resume():
+    return render_template("resume.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
